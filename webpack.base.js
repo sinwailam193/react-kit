ab8c87ddbb47a2dae17b7010ca2f2b67f840bb3e
sinwailam193 <aaronsinwailam@gmail.com>
2017-10-17 16:28:33 -0700

    change commands around

--- webpack.base.js
+++ webpack.base.js
@@ -10,15 +10,7 @@ module.exports = {
             {
                 test: /\.js?$/,
                 loader: "babel-loader",
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        "react",
-                        "es2015",
-                        "stage-0",
-                        ["env", { targets: { browsers: ["last 2 versions"] } }]
-                    ]
-                }
+                exclude: /node_modules/
             },
             {
                 test: /\.css$/,
